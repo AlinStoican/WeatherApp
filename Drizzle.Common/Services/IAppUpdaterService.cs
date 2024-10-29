b6eacf25f8516961745385adcb67606fcdd96d12
AlinStoican <132686402+AlinStoican@users.noreply.github.com>
2024-10-29 21:42:44 +0200

    Add files via upload

---
+++ Drizzle.Common/Services/IAppUpdaterService.cs
@@ -0,0 +1,18 @@
+﻿using Drizzle.Models.Enums;
+using System;
+using System.Threading.Tasks;
+
+namespace Drizzle.Common.Services;
+
+public interface IAppUpdaterService
+{
+    /// <summary>
+    /// Utc time since last update checked succesfully.
+    /// </summary>
+    DateTime LastCheckedTime { get; }
+    AppUpdateStatus LastCheckedStatus { get; }
+    Task<AppUpdateStatus> CheckUpdateAsync();
+    void Start();
+
+    event EventHandler<AppUpdateStatus> UpdateChecked;
+}
---
+++ Drizzle.Common/Services/ISystemInfoProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drizzle.Common.Services
+{
+    public interface ISystemInfoProvider
+    {
+        /// <summary>
+        /// Returns true is the current device is a Desktop.
+        /// </summary>
+        bool IsDesktop { get; }
+
+        /// <summary>
+        /// Returns true is the current
+        /// device is Xbox or other device
+        /// optimized for a 10-foot viewing
+        /// distance.
+        /// </summary>
+        bool IsTenFoot { get; }
+
+        bool IsHardwareAccelerated { get; }
+
+        public bool IsFirstRun { get; }
+
+        public bool IsAppUpdated { get; }
+
+        public string AppName { get; }
+
+        public Version AppVersion { get; }
+
+        public string GpuName { get; }
+    }
+}
